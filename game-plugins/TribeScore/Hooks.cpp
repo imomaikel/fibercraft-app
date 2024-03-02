3e9884787a76fd37b4a1c5882a6ea4c0a735090d
ivanlr-design <141946852+ivanlr-design@users.noreply.github.com>
2024-03-02 21:06:07 +0100

    feat(plugin): add Character Die Hook

--- game-plugins/TribeScore/Hooks.cpp
+++ game-plugins/TribeScore/Hooks.cpp
@@ -10,7 +10,7 @@ namespace TribeScore::Hooks {
     DECLARE_HOOK(AShooterGameMode_Logout, void, AShooterGameMode*, AController*);
     DECLARE_HOOK(APrimalStructure_Die, bool, APrimalStructure*, float, FDamageEvent*, AController*, AActor*);
     DECLARE_HOOK(AShooterGameMode_StartNewShooterPlayer, void, AShooterGameMode*, APlayerController*, bool, bool, FPrimalPlayerCharacterConfigStruct*, UPrimalPlayerData*);
-
+    DECLARE_HOOK(AShooterCharacter_Die, bool, AShooterCharacter*, float, FDamageEvent*, AController*, AActor*);
 
     // Login Hook
     void Hook_AShooterGameMode_StartNewShooterPlayer(AShooterGameMode* game, APlayerController* playerController, bool forceCreateNewPlayerData, bool isFromLogin, FPrimalPlayerCharacterConfigStruct* config, UPrimalPlayerData* data) {
@@ -40,6 +40,47 @@ namespace TribeScore::Hooks {
         }
     }
 
+    // Kill Player Hook
+    bool Hook_AShooterCharacter_Die(AShooterCharacter* _this, float KillingDamage, FDamageEvent* DamageEvent, AController* Killer, AActor* DamageCauser) {
+        
+        const int AttackerId = Killer->TargetingTeamField();
+        const int DefenderId = _this->TargetingTeamField();
+
+        // Kill someone of your tribe
+        if (AttackerId == DefenderId) return AShooterCharacter_Die_original;
+        
+        std::string attacker = std::to_string(AttackerId);
+        std::string defender = std::to_string(DefenderId);
+
+        TribeScore::database->UpdateTribeScore(attacker, defender, std::to_string(50));
+
+        const auto& actorsInRange = ArkApi::GetApiUtils().GetAllActorsInRange(_this->RootComponentField()->RelativeLocationField(), 20000.0f, EServerOctreeGroup::PLAYERS_CONNECTED);
+        for (AActor* actorInRange : actorsInRange) {
+            const auto aController = actorInRange->GetInstigatorController();
+            AShooterPlayerController* playerController = reinterpret_cast<AShooterPlayerController*>(aController);
+
+            const std::string textPoints = std::to_string(50);
+            FString floatingText = FString("+ " + textPoints + " tribe score");
+
+            uint64 steamId = ArkApi::GetApiUtils().GetSteamIdFromController(playerController);
+            std::string textSteamId = std::to_string(steamId);
+            const bool found = Commands::isSteamDisabled(textSteamId);
+
+
+            if (!playerController) return APrimalStructure_Die_original;
+            if (found == true) return APrimalStructure_Die_original;
+
+            if (actorInRange->TargetingTeamField() == DefenderId) {
+                playerController->ClientAddFloatingText(_this->RootComponentField()->RelativeLocationField(), &floatingText, FColor(255, 0, 0, 255), 0.6, 0.6, 6, FVector(0.2, 0.2, 0.2), 1, 0.5, 0.5);
+            } else if (actorInRange->TargetingTeamField() == AttackerId) {
+                playerController->ClientAddFloatingText(_this->RootComponentField()->RelativeLocationField(), &floatingText, FColor(0, 255, 0, 255), 0.6, 0.6, 6, FVector(0.2, 0.2, 0.2), 1, 0.5, 0.5);
+            } else {
+                playerController->ClientAddFloatingText(_this->RootComponentField()->RelativeLocationField(), &floatingText, FColor(255, 177, 0, 255), 0.6, 0.6, 6, FVector(0.2, 0.2, 0.2), 1, 0.5, 0.5);
+            }
+        }
+
+        return AShooterCharacter_Die_original(_this, KillingDamage, DamageEvent, Killer, DamageCauser);
+    }
 
     // Structure die Hook
     bool Hook_APrimalStructure_Die(APrimalStructure* _this, float damage, FDamageEvent* damageEvent, AController* controller, AActor* actor) {
@@ -52,8 +93,8 @@ namespace TribeScore::Hooks {
 
         auto attackerTribeId = actor->TargetingTeamField();
         auto attackerId = actor->TargetingTeamField();
-        
-        // Destroyed own structure
+
+        // Destroyed own structure        
         if (destroyedTribeId == attackerTribeId) return APrimalStructure_Die_original;
 
         /// Unclaimed dinos
@@ -110,11 +151,13 @@ namespace TribeScore::Hooks {
         ArkApi::GetHooks().SetHook("APrimalStructure.Die", &Hook_APrimalStructure_Die, &APrimalStructure_Die_original);
         ArkApi::GetHooks().SetHook("AShooterGameMode.Logout", &Hook_AShooterGameMode_Logout, &AShooterGameMode_Logout_original);
         ArkApi::GetHooks().SetHook("AShooterGameMode.StartNewShooterPlayer", &Hook_AShooterGameMode_StartNewShooterPlayer, &AShooterGameMode_StartNewShooterPlayer_original);
+        ArkApi::GetHooks().SetHook("AShooterCharacter.Die", &Hook_AShooterCharacter_Die,&AShooterCharacter_Die_original);
     }
 
     void Unload() {
         ArkApi::GetHooks().DisableHook("APrimalStructure.Die", &Hook_APrimalStructure_Die);
         ArkApi::GetHooks().DisableHook("AShooterGameMode.Logout", &Hook_AShooterGameMode_Logout);
         ArkApi::GetHooks().DisableHook("AShooterGameMode.StartNewShooterPlayer", &Hook_AShooterGameMode_StartNewShooterPlayer);
+        ArkApi::GetHooks().DisableHook("AShooterCharacter.Die", &Hook_AShooterCharacter_Die);
     }
 }
