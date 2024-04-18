173b00995e75239250844f0e6079d59c5553efd2
imomaikel <imomaikeldev@gmail.com>
2024-04-18 23:27:23 +0200

    refactor(website): add nav links type

--- app/(assets)/lib/constans.ts
+++ app/(assets)/lib/constans.ts
@@ -1,6 +1,14 @@
 import { ManagementPermission } from '@prisma/client';
 
-export const NAV_LINKS = [
+export const NAV_LINKS: {
+  label: string;
+  itemsOnHover: {
+    label: string;
+    path: `/${string}`;
+    description: string;
+    permission: ManagementPermission;
+  }[];
+}[] = [
   {
     label: 'Management',
     itemsOnHover: [
@@ -8,37 +16,37 @@ export const NAV_LINKS = [
         label: 'Permissions',
         path: '/management/permissions',
         description: 'Manage access to the management panel.',
-        permission: 'ALL_PERMISSIONS' as ManagementPermission,
+        permission: 'ALL_PERMISSIONS',
       },
       {
         label: 'Discord Selection',
         path: '/management/discord-selection',
         description: 'Select a Discord server that you want to control.',
-        permission: 'USE_WIDGETS' as ManagementPermission,
+        permission: 'USE_WIDGETS',
       },
       {
         label: 'Server Control',
         path: '/management/server-control',
         description: 'Manage in-game servers here.',
-        permission: 'SERVER_CONTROL' as ManagementPermission,
+        permission: 'SERVER_CONTROL',
       },
       {
         label: 'Paired Accounts',
         path: '/management/paired-accounts',
         description: 'Search for in-game names or Steam IDs',
-        permission: 'VIEW_PAIRED_ACCOUNTS' as ManagementPermission,
+        permission: 'VIEW_PAIRED_ACCOUNTS',
       },
       {
         label: 'Widgets',
         path: '/management/widgets',
         description: 'Configure the most important widget settings.',
-        permission: 'USE_WIDGETS' as ManagementPermission,
+        permission: 'USE_WIDGETS',
       },
       {
         label: 'Panel logs',
         path: '/management/panel-logs',
         description: 'View all panel actions taken by management.',
-        permission: 'VIEW_LOGS' as ManagementPermission,
+        permission: 'VIEW_LOGS',
       },
       {
         label: 'Advanced Search',
