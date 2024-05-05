d7afa968b8c7aefa12e6717e8f6b01365351701e
imomaikel <imomaikeldev@gmail.com>
2024-05-06 00:51:14 +0200

    feat(website): product card ui improvements

--- app/(store)/store/components/Product.tsx
+++ app/(store)/store/components/Product.tsx
@@ -1,9 +1,10 @@
 'use client';
+import AddToCartWrapper from '@assets/components/cart/AddToCartWrapper';
 import { CardBody, CardContainer, CardItem } from '@ui/3d-card';
 import { MdKeyboardArrowRight } from 'react-icons/md';
+import { motion, useAnimate } from 'framer-motion';
 import { formatPrice } from '@assets/lib/utils';
 import { Skeleton } from '@ui/skeleton';
-import { motion } from 'framer-motion';
 import { Button } from '@ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -22,6 +23,7 @@ const Product = ({ description, id, name, image, totalPrice }: TProduct) => {
   if (description.length >= 3) {
     description += '...';
   }
+  const [scope, animate] = useAnimate();
 
   return (
     <motion.div
@@ -38,23 +40,28 @@ const Product = ({ description, id, name, image, totalPrice }: TProduct) => {
       viewport={{ once: true }}
     >
       <CardContainer containerClassName="h-full" className="h-full">
-        <CardBody className="group/card relative flex h-full w-auto flex-col justify-around rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black/50 dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]">
-          <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
-            {name}
-          </CardItem>
-          <CardItem as="p" translateZ="60" className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300">
-            {description}
-          </CardItem>
-          <CardItem translateZ="100" className="mt-4 w-full">
-            <Image
-              src={image || '/empty.png'}
-              height="1000"
-              width="1000"
-              className="h-60 w-full rounded-xl object-contain group-hover/card:shadow-xl"
-              alt={`${name} image`}
-            />
-          </CardItem>
-          <div className="mt-8 flex items-center justify-between space-x-6">
+        <CardBody className="group/card relative flex h-full w-auto flex-col justify-around rounded-xl border border-black/[0.1] bg-gray-50 dark:border-white/[0.2] dark:bg-black/50 dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]">
+          <Link href={`/store/${id}`} className="flex h-full flex-col justify-around px-6 pt-6">
+            <CardItem
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 group-hover/card:text-primary dark:text-white"
+            >
+              {name}
+            </CardItem>
+            <CardItem as="p" translateZ="60" className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300">
+              {description}
+            </CardItem>
+            <CardItem translateZ="100" className="mt-4 w-full">
+              <Image
+                src={image || '/empty.png'}
+                height="1000"
+                width="1000"
+                className="h-60 w-full rounded-xl object-contain group-hover/card:shadow-xl"
+                alt={`${name} image`}
+              />
+            </CardItem>
+          </Link>
+          <div className="mt-8 flex items-center justify-between space-x-6 px-6 pb-6">
             <CardItem translateZ={20} as={Link} href={`/store/${id}`}>
               <Button variant="ghost">
                 See the full product <MdKeyboardArrowRight className="h-8 w-8" />
@@ -64,7 +71,16 @@ const Product = ({ description, id, name, image, totalPrice }: TProduct) => {
               {formatPrice(totalPrice)}
             </CardItem>
             <CardItem translateZ={20} as="button">
-              <Button>Add to Cart</Button>
+              <AddToCartWrapper itemId={id} itemName={name}>
+                <div
+                  ref={scope}
+                  onClick={() => {
+                    animate(scope.current, { scale: [1, 1.1, 0.9, 1] }, { duration: 0.4 });
+                  }}
+                >
+                  <Button>Add to Cart</Button>
+                </div>
+              </AddToCartWrapper>
             </CardItem>
           </div>
         </CardBody>
