b7be233e1c1048e7be47ec30b4766a54f4b6d295
imomaikel <imomaikeldev@gmail.com>
2024-05-23 22:18:38 +0200

    feat(website): limit the testimonials' height

--- app/(assets)/components/ui/infinite-moving-cards.tsx
+++ app/(assets)/components/ui/infinite-moving-cards.tsx
@@ -83,7 +83,7 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item) => (
           <li
-            className="relative w-[350px] max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="testimonialScroll relative max-h-[300px] w-[350px] max-w-full flex-shrink-0 overflow-y-auto overflow-x-hidden rounded-2xl rounded-br-sm rounded-tr-sm border border-b-0 border-slate-700 px-8 py-6 md:w-[450px]"
             style={{
               background: 'linear-gradient(180deg, var(--slate-800), var(--slate-900)',
             }}
@@ -92,9 +92,11 @@ export const InfiniteMovingCards = ({
             <blockquote>
               <div
                 aria-hidden="true"
-                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-full w-[calc(100%_+_4px)]"
               ></div>
-              <span className="relative z-20 text-sm font-normal leading-[1.6] text-gray-100">{item.content}</span>
+              <span className="relative z-20 whitespace-pre-wrap text-sm font-normal leading-[1.6] text-gray-100">
+                {item.content}
+              </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <span className=" text-sm font-normal leading-[1.6] text-gray-400">{item.discordUsername}</span>
